6e27c226eedaef089be46f8b3119152d8f5d18ff
Aman Nagar <amannagar4870@gmail.com>
2024-05-31 18:49:18 +0530

    add to cart

--- src/components/Pages/Product/ProductPage.jsx
+++ src/components/Pages/Product/ProductPage.jsx
@@ -2,13 +2,13 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../../Context/AppContext";
-import ProductSlider from "../../Slider/ProductSlider";
 import "./ProductPage.scss";
 import ProductPageSimmer from "../../SimmerEffect/ProductSimmer/ProductPageSimmer";
+import { Toaster, toast } from "sonner";
 
 export default function ProductPage() {
   const { id } = useParams();
-  const { allProducts } = useContext(AppContext);
+  const { allProducts, addToCart } = useContext(AppContext);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -17,26 +17,35 @@ export default function ProductPage() {
   }
 
   const product = allProducts.find((product) => product.id == id);
-  console.log(product);
   if (!product) {
     return <div>Product not Found</div>;
   }
 
+  const handleAddToCart = () => {
+    addToCart(product);
+    toast.success(`${product.title} has been added to the cart!`);
+  };
+
   return (
-    <div className="product-page">
-      <div className="p-images">
-        <img src={product.images[0]} alt="" />
-      </div>
-      <div className="p-info">
-        <div className="p-details">
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
+    <>
+      <div className="product-page">
+        <div className="p-images">
+          <img src={product.images[0]} alt="" />
         </div>
-        <div className="price">
-          <p>Price ${product.price}</p>
-          <button className="add-to-cart">Add To Cart</button>
+        <div className="p-info">
+          <div className="p-details">
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+          </div>
+          <div className="price">
+            <p>Price ${product.price}</p>
+            <button className="add-to-cart" onClick={handleAddToCart}>
+              Add To Cart
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
